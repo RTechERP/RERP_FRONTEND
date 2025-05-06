a33b40a84145e090ce73700e607f6bb9587f536e
Admin <Admin@DESKTOP-40H717B>
2025-05-06 16:40:16 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { EmployeesComponent } from './components/employees/employees.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, EmployeesComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'R_ERP';
+    title = 'R_ERP';
+    @ViewChild('container', { read: ViewContainerRef }) container!: ViewContainerRef
+
+
+    addComponent() {
+        this.container.clear(); // (nếu muốn xóa trước)
+        this.container.createComponent(EmployeesComponent);
+    }
 }
+
