3949bc05dd023c41afd22be3f236852dc592d01d
namvu <namvu@NAM>
2025-05-07 14:55:02 +0700

    project update

---
+++ src/app/components/projects/project-search/project-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-search',
+  imports: [],
+  templateUrl: './project-search.component.html',
+  styleUrl: './project-search.component.css'
+})
+export class ProjectSearchComponent {
+
+}
