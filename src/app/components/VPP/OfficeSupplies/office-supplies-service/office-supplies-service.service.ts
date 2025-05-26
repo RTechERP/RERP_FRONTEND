3915a1fed88dc33d4bda3d5f2d46960263c9a0f1
nhubinh2104 <92156609+Binh2104@users.noreply.github.com>
2025-05-26 14:08:46 +0700

    update: update URL

--- src/app/components/VPP/OfficeSupplies/office-supplies-service/office-supplies-service.service.ts
+++ src/app/components/VPP/OfficeSupplies/office-supplies-service/office-supplies-service.service.ts
@@ -1,45 +1,45 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+// import { API_URL } from '../../../../app.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfficeSuppliesService {
-  private baseUrl = 'https://localhost:7187/api/OfficeSupplies';
-
+  private baseUrl = `${'https://localhost:7187'}/api/OfficeSupplies`;
   constructor(private httpclient: HttpClient) { }
 
   getdata(keyword: string): Observable<any> {
-    return this.httpclient.get<any>(`${this.baseUrl}/getall?keyword=${encodeURIComponent(keyword)}`);
+    return this.httpclient.get<any>(`${this.baseUrl}/getdataofficesupplies?keyword=${encodeURIComponent(keyword)}`);
   }
 
   getUnit(): Observable<any> {
-    return this.httpclient.get<any>(`https://localhost:7187/api/OfficeSupplies/GetOfficeSupplyUnit`);
+    return this.httpclient.get<any>(`${this.baseUrl}/getdataofficesupplyunit`);
   }
 
   addUnit(data: any): Observable<any> {
-    return this.httpclient.post<any>('https://localhost:7187/api/OfficeSupplies/savedatas',data);
+    return this.httpclient.post<any>(`${this.baseUrl}/savedatofficesupplyunit`, data);
   }
 
   getdatafill(id: number): Observable<any> {
-    return this.httpclient.get(`${this.baseUrl}/${id}`);
+    return this.httpclient.get(`${this.baseUrl}/getbyidofficesupplies?id=${id}`);
   }
 
   adddata(data: any): Observable<any> {
-    return this.httpclient.post(`${this.baseUrl}/themVPP`, data);
+    return this.httpclient.post(`${this.baseUrl}/addandupdate`, data);
   }
 
   updatedata(data: any): Observable<any> {
-    return this.httpclient.post(`${this.baseUrl}/capnhatVPP`, data);
+    return this.httpclient.post(`${this.baseUrl}/adddandupdate`, data);
   }
 
   deletedata(ids: number[]): Observable<any> {
-    return this.httpclient.post(`${this.baseUrl}/delete-vpp`, ids);
+    return this.httpclient.post(`${this.baseUrl}/deleteofficesupply`, ids);
   }
 
   searchdata(id: number): Observable<any> {
-    return this.httpclient.get(`${this.baseUrl}/${id}`);
+    return this.httpclient.get(`${this.baseUrl}/getbyidofficesupplies?id=${id}`);
   }
   nextCodeRTC(): Observable<any>{
     return this.httpclient.get<any>(`${this.baseUrl}/next-codeRTC`, { responseType: 'text' as 'json' });
--- src/app/components/VPP/OfficeSupplyUnit/OSU-service/office-supply-unit-service.service.ts
+++ src/app/components/VPP/OfficeSupplyUnit/OSU-service/office-supply-unit-service.service.ts
@@ -8,13 +8,13 @@ export class OfficeSupplyUnitServiceService {
 
    constructor(private httpclient: HttpClient) { }
   getdata(): Observable<any> {
-    return this.httpclient.get<any>(`https://localhost:7187/api/OfficeSupplies/GetOfficeSupplyUnit`);
+    return this.httpclient.get<any>(`https://localhost:7187/api/OfficeSupplies/getdataofficesupplyunit`);
   }
   updatedata(data:any): Observable<any>{
-    return this.httpclient.post<any>('https://localhost:7187/api/OfficeSupplies/savedatas',data);
+    return this.httpclient.post<any>('https://localhost:7187/api/OfficeSupplies/savedatofficesupplyunit',data);
   }
    getdatafill(id:number):Observable<any>{
-    return this.httpclient.get('https://localhost:7187/api/OfficeSupplies/getbyid?id='+id);
+    return this.httpclient.get('https://localhost:7187/api/OfficeSupplies/getbyidofficesupplyunit?id='+id);
    }
    deletedata(id:number[]):Observable<any>{
     return this.httpclient.post('https://localhost:7187/api/OfficeSupplies/deleteOfficeSupplyUnit', id);
